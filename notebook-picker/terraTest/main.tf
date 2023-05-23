72d2b392ea710113982939f8a4e5000e17c930e4
Lynn Langit <lynnlangit@gmail.com>
2023-05-22 21:09:51 -0500

    updated conditional example

--- notebook-picker/terraTest/main.tf
+++ notebook-picker/terraTest/main.tf
@@ -1,22 +1,37 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.51.0"
     }
   }
 }
 
 provider "google" {
-    
+
   # locally use 'gcloud auth application-default login' to authenticate  
   # credentials = file("<NAME>.json")
 
-    project = var.project
-    region  = var.region
-    zone    = var.zone
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
+}
+resource "google_compute_instance" "conditional_test" {
+  name         = "conditional-test"
+  machine_type = var.machine_type
+  zone         = var.zone
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
 }
--- notebook-picker/terraTest/variables.tf
+++ notebook-picker/terraTest/variables.tf
@@ -1,5 +1,12 @@
 variable "project" {
-    default = "genomics-compute"
+  default     = "genomics-compute"
+  description = "name of the project to deploy to"
+  type        = string
+
+  validation {
+    condition     = length(var.project) > 0
+    error_message = "This application requires a project name."
+  }
 }
 
 variable "region" {
@@ -8,4 +15,24 @@ variable "region" {
 
 variable "zone" {
   default = "us-central1-c"
+}
+
+variable "machine_type" {
+  description = "Machine type to use for the notebook instance"
+  type        = string
+  default     = "n1-standard-1"
+
+  validation {
+    # validate that the machine_type is one of the allowed values from the list of machine_types
+    condition     = contains([
+        "n1-standard-1", 
+        "n1-standard-2", 
+        "n1-standard-4", 
+        "n1-standard-8", 
+        "n1-standard-16", 
+        "n1-standard-32", 
+        "n1-standard-64", 
+        "n1-standard-96"], var.machine_type)
+    error_message = "This application requires a valid machine type."
+  }
 }
